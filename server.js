9c1109e4502aad2407367242b557b5f80074aaea
TGAP4 <60671900+TGAP4@users.noreply.github.com>
2020-05-26 02:36:51 -0400

    fixed server issue

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(express.static('build/'));
+app.use(express.static('build'));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build/', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 3000);
